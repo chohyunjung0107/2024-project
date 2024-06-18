c34322e52f857fa0260ae55db645c908e1b291a4
hyunjung <hyunjung.cho@altiall.com>
2024-06-18 16:35:55 +0900

    배포테스트2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // output: "export",
-  // assetPrefix: process.env.NODE_ENV === "production" ? "/2024-project/" : "",
-};
+const nextConfig = {};
 
 export default nextConfig;
