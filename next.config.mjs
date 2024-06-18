3c9f29cf16e4eeef54e29cee0ee9dc757d98e0f2
hyunjung <hyunjung.cho@altiall.com>
2024-06-18 15:34:12 +0900

    t

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/2024-project/" : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://chohyunjung0107.github.io/2024-project/"
+      : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
