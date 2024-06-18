a67cf90b43d8e8cb2326c1ea6b2ec0364de23570
hyunjung <hyunjung.cho@altiall.com>
2024-06-18 17:07:13 +0900

    배포테스트5

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>여긴 home</h1>
+      <h1>한재호바아보~</h1>
       {/* <ul>
         <li>
           <a href="/list">리스트 테스트1</a>
