5752819716992c813c5f114347724c82f546e3b5
hyunjung <hyunjung.cho@altiall.com>
2024-06-19 09:44:03 +0900

    cors에러 대처5

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
+  async headers() {
     return [
       {
-        // 이 예시는 모든 페이지에 CORS 설정을 적용합니다.
-        source: '/(.*)',
+        source: "/api/:path*",
         headers: [
           {
-            key: 'Access-Control-Allow-Credentials',
-            value: 'true',
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
           },
           {
-            key: 'Access-Control-Allow-Origin',
-            value: '*', // 필요에 따라 원본을 지정하십시오.
+            key: "Access-Control-Allow-Origin",
+            value: "*", // 필요에 따라 특정 Origin을 설정
           },
           {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,OPTIONS",
           },
           {
-            key: 'Access-Control-Allow-Headers',
-            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
           },
         ],
       },
