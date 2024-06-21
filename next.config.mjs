209199ebc7bfb97017a3758f2008dc4799593eef
hyunjung <hyunjung.cho@altiall.com>
2024-06-21 10:03:58 +0900

    빌드에러 대응

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // 필요에 따라 특정 Origin을 설정
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,POST,OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
